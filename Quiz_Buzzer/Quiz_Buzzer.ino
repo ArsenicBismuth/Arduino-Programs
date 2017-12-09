6fa3ef72ebb7139a08264e79e02d5214212dc26e
Unknown <d.faris323@gmail.com>
2017-12-09 14:18:10 +0700

    Buzzer Completion
    Using internal pull-up in analog pins as input.
    System is complete.

--- Quiz_Buzzer/Quiz_Buzzer.ino
+++ Quiz_Buzzer/Quiz_Buzzer.ino
@@ -1,40 +1,75 @@
 #include "pitches.h"
+#include <stdint.h>
+
+
+// [Config] Basic settings
+#define BUTTON 5        // Button count
+#define DUR_AFTER 3000  // Duration after buzzing before a new button press permitted
+#define TH_BUTTON 256   // analogRead threshold for
+#define DEBUG 0         // wether to enable playMelody or not
+
+// The components are all active LOW
+const uint8_t pin_button[BUTTON] = {A0, A1, A0, A0, A0};
+const uint8_t pin_output[BUTTON] = {3, 5, 3, 3, 3};
 
 //Notes in the melody: (note durations: 4 = quarter note, 8 = eighth note, etc.)
 typedef struct note {
-  int key; int dur;
-} melody[] = {
-  //{NOTE_C4, 4}, {NOTE_G3, 8}, {NOTE_G3, 8}, {NOTE_A3, 4}, {NOTE_G3, 4}, {0, 4}, {NOTE_B3, 4}, {NOTE_C4, 4}
-  //{NOTE_C4, 4}, {NOTE_D4, 4}, {NOTE_E4, 4}, {NOTE_F4, 4}, {NOTE_G4, 4}, {NOTE_A4, 4}, {NOTE_B4, 4}, {NOTE_C5, 4}, {NOTE_D5, 4}, {NOTE_E5, 4}, {NOTE_F5, 4}
-  {NOTE_E5, 1}
+  int key; float dur;
 };
 
-int pin_buzzer = 3;
+note melody[] = {
+  //{NOTE_C4, 4}, {NOTE_G3, 8}, {NOTE_G3, 8}, {NOTE_A3, 4}, {NOTE_G3, 4}, {0, 4}, {NOTE_B3, 4}, {NOTE_C4, 4}
+  //{NOTE_C4, 4}, {NOTE_D4, 4}, {NOTE_E4, 4}, {NOTE_F4, 4}, {NOTE_G4, 4}, {NOTE_A4, 4}, {NOTE_B4, 4}, {NOTE_C5, 4}, {NOTE_D5, 4}, {NOTE_E5, 4}, {NOTE_F5, 4}
+  //{NOTE_C5, 12}, {0, 12}, {NOTE_D5, 12}, {0, 12}, {NOTE_E5, 0.5}
+  {NOTE_E5, 1}, {0, 12}, {NOTE_E5, 12}, {0, 12}, {NOTE_E5, 12}
+  //{NOTE_E5, 0.5}
+  //{NOTE_C4, 1}
+};
 
-void playmelody(int repeat) {
+int duration;
+
+void playMelody(int repeat, int pin) {
   //Iterate over the notes of the melody:
   for (; repeat >= 0; repeat--) {
-    for (int i = 0; i < sizeof(melody) - 1; i++) {
-      //To calculate the note duration, take one second divided by the note type.
+    for (int i = 0; i < sizeof(melody)/sizeof(note); i++) {
+      //To calculate the note    n       , take one second divided by the note type.
       //e.g. quarter note = 1000/4, eighth note = 1000/8, etc.
       duration = 1000 / melody[i].dur;
-      tone(pin_buzzer, melody[i].key, duration);
+      tone(pin, melody[i].key, duration);
   
-      //To distinguish the notes, set a minimum time between them.
+      //To distinguish the notes, set a minn n imum time between them.
       //the note's duration + 30% seems to work well:
-      int pause = duration * 1.30;
+      //int pause = duration * 1.30;
+      int pause = duration * 1.10;
       delay(pause);
       
       //Stop the tone playing:
-      noTone(pin_buzzer);
+      noTone(pin);
     }
   }
 }
 
 void setup() {
-  
+  Serial.begin(9600);
+
+  for (int i = 0; i < BUTTON; i++){
+    pinMode(pin_button[i], INPUT_PULLUP);
+    pinMode(pin_output[i], OUTPUT);
+  }
 }
 
 void loop() {
-  
+  for (int i = 0; i < BUTTON; i++){
+    Serial.print(analogRead(pin_button[i]));
+    Serial.print(' ');
+    Serial.print(digitalRead(pin_button[i])*1000);
+    Serial.print(' ');
+    if (!DEBUG && (analogRead(pin_button[i]) < TH_BUTTON)) {
+      playMelody(0, pin_output[i]);
+      digitalWrite(pin_output[i], HIGH);  // Stays on until a period of time
+      delay(DUR_AFTER);
+      digitalWrite(pin_output[i], LOW);  // Stays on until a period of time
+    }
+  }
+  Serial.println();
 }
