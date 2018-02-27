84408bde92f6a4a21a287e4a92b9530523e36f74
Unknown <d.faris323@gmail.com>
2018-02-27 23:28:17 +0700

    Using 4x4 Keypad + ADC
    A bloat of upgrade, using 4x4 Keypad. Tested in Tinkercad to be fully functional. Fully.
    
    Every explanations + limitations commented.

--- AVR_Timer/AVR_Timer.ino
+++ AVR_Timer/AVR_Timer.ino
@@ -2,11 +2,14 @@
  * Original: http://www.avr-tutorials.com/projects/atmega16-based-digital-clock
  * Further Developed by Dafa Faris Muhammad & Daniel Steven
  *
- * TinkerCAD: https://www.tinkercad.com/things/819XeKP5d56-neat-snicket/editel?sharecode=WGq3tsCr5HTkRXMRe8cOgkFx0VyLV-NySdcAKFX-mCI=
+ * TinkerCAD: https://www.tinkercad.com/things/2AZw3X21r0k-copy-of-neat-snicket/editel?sharecode=ebuvlFIe8aLPWZyW79dfbpua2jYm88cSwJwrc6oba88=
+ * Keypad Resistors: http://www.microchip.com/forums/m/tm.aspx?m=694613&p=1
+ * ADC Reference: http://maxembedded.com/2011/06/the-adc-of-the-avr/
  */ 
  
 #define F_CPU 16000000UL		// 16MHz, default in most Arduino
 
+#include <stdint.h> 
 #include <avr/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -31,12 +34,9 @@
 #define ButPin			PINB
 #define ButDDR			DDRB
 */
-
-// For data reading, below is legal
-#define ButPin			(PINB >> 2)
  
 /*Global Variables Declarations*/
-unsigned char SegDataPort = 0;
+uint8_t SegDataPort = 0;
 /* 
 * Port combiner
 * Combine PORTD[7:2] & PORTB[1:0]
@@ -47,17 +47,23 @@ unsigned char SegDataPort = 0;
 *	PORTB = (SegDataPort & 0b00000011);
 */
 
-unsigned char hours = 0;
-unsigned char minutes = 0;
-unsigned char seconds = 0;
+#define PINADC 6		// Use A6, only available in SMD ATmega
+#define BUTTER 3		// Allowable ADC error from specified button mapping value
 
-unsigned char int1 = 0;		// Leftmost number, to be operated against
-unsigned char int2 = 0;
-unsigned char result = 0;
-unsigned char curs = 0;		// Specify cursor location, displayed using the period
+uint8_t hours = 0;
+uint8_t minutes = 0;
+uint8_t seconds = 0;
+uint8_t curs = 0;		// Specify cursor location, displayed using the period
 
-unsigned char pbut = 0xFF;	// Previous button state
-unsigned char calc = 0;		// Calculator modes, total of 5: inactive, sum, reduce, multi, div
+int16_t intm = 0;		// Main integer to be displayed & operated on non-clock mode
+
+uint16_t inta = 0;		// Integers ONLY to store calculator data
+uint16_t intb = 0;
+uint8_t sign = 0;		// Negative or not sign
+
+uint8_t but = 0xFF;		// Button mapped from ADC reading
+uint8_t pbut = 0;		// Previous button state
+uint8_t mode = 0;		// Modes, total of 5: clock, set clock, calculation modes (+, -, *, /), enter
  
 /*
 * Basic timer concepts
@@ -74,13 +80,25 @@ unsigned char calc = 0;		// Calculator modes, total of 5: inactive, sum, reduce,
 /*Function Declarations*/
 /*****************************************************************************/
 /*Decimal Digit (0-9) to Seven Segment Values Encoder*/
-unsigned char DigitTo7SegEncoder(unsigned char digit, unsigned char common);
+uint8_t DigitTo7SegEncoder(uint8_t digit, uint8_t common);
 
-/*Increment any of six digits based on location*/
-unsigned char Increment(unsigned char digit);
+/*Update 7-segment display*/
+void UpdateDisplay(uint8_t d5, uint8_t d4, uint8_t d3, uint8_t d2, uint8_t d1, uint8_t d0, uint8_t dot);
 
 /*Update combined ports used by the 7-segments*/
-unsigned char UpdateMultiPorts();
+void UpdateMultiPorts();
+
+/*Read analog data using ADC*/
+uint16_t AnalogRead(uint8_t ch);
+
+/*Map Buttons from ADC Reading*/
+uint8_t Button(uint8_t adc, uint8_t error);
+
+/*Compare with error*/
+uint8_t InRange(uint8_t ch);
+
+/*Absolute*/
+uint16_t Abs(int16_t in);
  
 /*Timer Counter 1 Compare Match A Interrupt Service Routine/Interrupt Handler*/
 ISR(TIMER1_COMPA_vect);
@@ -90,140 +108,156 @@ ISR(TIMER1_COMPA_vect);
 /*****************************************************************************/
 int main(void)
 {	
-	// Serial.begin(9600);
+	Serial.begin(9600);
 	
-	// MCU parameter configs
+	/* MCU parameter configs */
+	// Pins initialization
 	DDRD = 0b11111100;	// Output 7-segment data [7:2]
 	DDRB = 0b00000011;	// Output 7-segment data [1:0], Input [7:2]
 	
-	PORTD = 0xFF;		// Enable pullup on input, set HIGH for output
-	PORTB = 0xFF;		// Enable pullup on input, set HIGH for output
+	PORTD = 0xFF;			// Enable pullup on input, set HIGH for output
+	PORTB = 0xFF;			// Enable pullup on input, set HIGH for output
 	
 	DDRC = 0xFF;		// Output 7-segment common pin (control)
-	PORTC = 0xFF;		// HIGH 7-segment common pin
+	PORTC = 0xFF;	// HIGH 7-segment common pin
  
+	// Timer initialization
 	// TCNT1						// Background variable which counts system clock ticks after modified by Prescaler 1
 	TCCR1B = (1<<CS12|1<<CS10|1<<WGM12);	// Clock Select 1, prescaler set as 1/1024. // Clear Time on Compare 1, set Timer/Counter mode to CTC
 	OCR1A = 15625-1;				// Ouput Compare Match, value to be referenced when comparing
 	TIMSK1 = 1<<OCIE1A;				// Timer/Counter1, Output Compare A Match Interrupt enable
 	sei();							// Set global interrupt flag
+	
+	// ADC initialization
+    ADMUX = (1<<REFS0);	// AREF = AVcc
+ 
+    // ADC Enable and prescaler of 128
+    // 16000000/128 = 125000
+    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
  
 	while(1) {
 		/*
 		* Basic Operations
-		* Buttons: [Move cursor] [Increment digit] [Cycle modes]
+		* Buttons: Change modes (A-D), digits (0-9), erase (*), & clear (#)
 		* - Buttons handled only on "Rising", preventing multiple strokes.
 		* - Clock functions normally even while being used as a calculator.
-		* - Every calculation works on-the-fly, any change of state will
-		* directly reflected on result
-		* - Cursor functionality allows for faster number manipulation,
-		* digit selected by cursor (marked by a dot) is the increment target
-		* instead of the number as a whole.
+		* Modes:
+		* - [A] Clock		(mode 0)
+		* - [B] Set Clock	(mode 1) Can't adjust the last two seconds due to int16 limitation
+		* - [C] Calculator	(mode 2 input A, mode 3 input B, mode 4-7 various results)
+		* - [D] Enter		(apply & increment calculator modes)
 		*/
 		
-		// Basic multi buttons management
-		for(unsigned char i = 0; i < 3; i++) {
-			
-			// Check rising, button isn't active while previously it's
-			if(!(~ButPin & (1<<i)) && (~pbut & (1<<i))) {
-				
-				switch(i) {
-					case 2:	// Cycle between multiple modes
-						if(calc < 4) calc++;
-						else calc = 0;
-						break;
-					case 1:	// Increment based on cursor position
-						Increment(curs);
-						break;
-					case 0:	// Move cursor
-						if (curs < 5) curs++;
-						else curs = 0;
-						break;
-					default:
-						break;
-				}
+		/*Button & mode handling*/
+		// Basic multi buttons management from single ADC pin
+		but = Button(AnalogRead(PINADC), BUTTER);
+		
+		// Check rising, button isn't active while previously it's
+		if((but == 255) && (pbut != 255)) {
+			switch(pbut) {					// [Important] Read the previous button, not current
+				// Mode handling buttons
+				case 'a':	// Clock
+					intm = 0;
+					mode = 0;
+					break;
+				case 'b':	// Set clock
+					intm = 0;
+					mode = 1;
+					break;
+				case 'c':	// Calculator
+					mode = 2;
+					intm = 0;
+					inta = 0;
+					intb = 0;
+					break;
+				case 'd':	// Enter
+					switch (mode) {
+						case 1:	// Set clock
+							/*seconds = intm%100;
+							intm /= 100;*/
+							minutes = intm%100;
+							intm /= 100;
+							hours = intm%100;
+							intm = 0;
+							mode = 0;
+							break;
+						case 2:	// Set first calculator number
+							inta = intm;
+							intm = 0;
+							mode++;
+							break;
+						case 3:	// Set second calculator number
+							intb = intm;
+							intm = 0;
+							mode++;
+							break;
+						case 4:
+						case 5:
+						case 6: mode++; break;	 // Go to the next calculation mode
+						case 7: mode = 2; break; // Loop again, to review
+						default: break;
+					}
+					break;
+				// Non-mode buttons
+				case '*': if (intm > 0) intm --; break;		// Decrement
+				case '#': intm = 0; break;					// Clear
+				// Normal input, 0-9. Limit input to 4 digits
+				default: if (intm <= 9999) intm = intm * 10 + pbut; break;
 			}
-
 		}
-        pbut = ButPin;	// Store button state
+        pbut = but;	// Store button state
 		
-		/* Serial.print(ButPin,BIN);
-		Serial.print("\n"); */
+		Serial.print(mode);
+		Serial.print(' ');
+		Serial.print(intm);
+		Serial.println();
 		
-		// Calculate result on-the-fly
-		switch(calc) {
-			case 1: result = int1 + int2; break;	// Summation
-			case 2: result = int1 - int2; break;	// Reduction
-			case 3: result = int1 * int2; break;	// Multiplication
-			case 4: result = int1 / int2; break;	// Division
-			default: result = 0; break;
+		/*Calculation handling*/
+		switch(mode) {
+			case 4: intm = inta + intb; break;	// Summation
+			case 5: intm = (int) inta - (int) intb; break;	// Reduction
+			case 6: intm = inta * intb; break;	// Multiplication
+			case 7: intm = inta / intb; break;	// Division
+			default: break;
 		}
 		
-		if(!calc) {
-			/* 
-			* Set one 7-segments at a time for every loop,
-			* fast enough to appear as if they're all always on.
-			* Isolation done by the Control Ports
-			*/
-			SegDataPort = DigitTo7SegEncoder(seconds%10,1) | ((1<<7) * (curs == 0));
-			UpdateMultiPorts();
-			PORTC = ~0x01;
-			/* Serial.print(SegDataPort,BIN);
-			Serial.print("\t");
-			Serial.print(PORTD,BIN);
-			Serial.print("\t");
-			Serial.print(PORTB,BIN);
-			Serial.print("\n"); */
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(seconds/10,1) | ((1<<7) * (curs == 1)); 
-			UpdateMultiPorts();
-			PORTC = ~0x02;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(minutes%10,1) | ((1<<7) * (curs == 2));
-			UpdateMultiPorts();
-			PORTC = ~0x04;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(minutes/10,1) | ((1<<7) * (curs == 3)); 
-			UpdateMultiPorts();
-			PORTC = ~0x08;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(hours%10,1) | ((1<<7) * (curs == 4)); 
-			UpdateMultiPorts();
-			PORTC = ~0x10;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(hours/10,1) | ((1<<7) * (curs == 5));
-			UpdateMultiPorts();
-			PORTC = ~0x20;
-			_delay_ms(1);
-		} else {
-			SegDataPort = DigitTo7SegEncoder(result%10,1) | ((1<<7) * (curs == 0));
-			UpdateMultiPorts();
-			PORTC = ~0x01;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(result/10,1) | ((1<<7) * (curs == 1)); 
-			UpdateMultiPorts();
-			PORTC = ~0x02;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(int2%10,1) | ((1<<7) * (curs == 2));
-			UpdateMultiPorts();
-			PORTC = ~0x04;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(int2/10,1) | ((1<<7) * (curs == 3)); 
-			UpdateMultiPorts();
-			PORTC = ~0x08;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(int1%10,1) | ((1<<7) * (curs == 4)); 
-			UpdateMultiPorts();
-			PORTC = ~0x10;
-			_delay_ms(1);
-			SegDataPort = DigitTo7SegEncoder(int1/10,1) | ((1<<7) * (curs == 5));
-			UpdateMultiPorts();
-			PORTC = ~0x20;
-			_delay_ms(1);
+		/*Display handling*/
+		
+		switch (mode)
+		{
+			case 0: UpdateDisplay(hours/10, hours%10, minutes/10, minutes%10, seconds/10, seconds%10, 2); break;
+			case 1: UpdateDisplay(intm/1000, intm%1000/100, intm%100/10, intm%10, seconds/10, seconds%10, 2); break;
+			case 2: UpdateDisplay('a', '=', intm/1000%10, intm/100%10, intm/10%10, intm%10, curs); break;
+			case 3: UpdateDisplay('b', '=', intm/1000%10, intm/100%10, intm/10%10, intm%10, curs); break;
+			case 4: 
+			case 5:
+			case 6:
+			case 7: 
+				sign = (intm < 0)*'-' | (intm >= 0)*' ';
+				intm = Abs(intm);
+				UpdateDisplay(mode-3, sign, intm/1000%10, intm/100%10, intm/10%10, intm%10, curs); break;
+			default: break;
 		}
- 
     }
 	return 0;
+}
+
+
+/*
+* Update 7-segment display
+* Ordered from leftmost to rightmost.
+* Set one 7-segments at a time for every loop,
+* fast enough to appear as if they're all always on.
+* Isolation done by the Control Ports
+*/
+void UpdateDisplay(uint8_t d5, uint8_t d4, uint8_t d3, uint8_t d2, uint8_t d1, uint8_t d0, uint8_t dot){
+	uint8_t digits[6] = {d0, d1, d2, d3, d4, d5};
+	for (int i = 0; i < 6; i++) {
+		SegDataPort = DigitTo7SegEncoder(digits[i], 1) | ((1<<7) * (dot == i));
+		UpdateMultiPorts();
+		PORTC = ~(0x01<<i) & ~0x1;
+		_delay_ms(1);
+	}
 }
 
 /* 
@@ -233,7 +267,7 @@ int main(void)
 *	SegDataPort = [D7...D2, B1...B0]
 *				= ((PORTD & 0b11111100) | (PORTB & 0b00000011));
 */
-unsigned char UpdateMultiPorts()
+void UpdateMultiPorts()
 {
 	// Get the output data from SegDataPort then combine with Pullup settings for inputs
 	PORTD = (SegDataPort & 0b11111100) | 0b00000011;
@@ -250,97 +284,31 @@ unsigned char UpdateMultiPorts()
 * Encoded SegVal is return in the order G-F-E-D-C-B-A with A as the least
 * significant bit (bit 0) and G bit 6.
 */
-unsigned char DigitTo7SegEncoder(unsigned char digit, unsigned char common)
+uint8_t DigitTo7SegEncoder(uint8_t digit, uint8_t common)
 {
-	unsigned char SegVal;
+	uint8_t SegVal;
  
 	switch(digit)	
 	{	
-		case 0:	if(common == 1)	SegVal = 0b00111111;
-				else			SegVal = ~0b00111111;
-				break;
-		case 1:	if(common == 1)	SegVal = 0b00000110;
-				else			SegVal = ~0b00000110;
-				break;
-		case 2:	if(common == 1)	SegVal = 0b01011011;
-				else			SegVal = ~0b01011011;
-				break;
-		case 3:	if(common == 1)	SegVal = 0b01001111;
-				else			SegVal = ~0b01001111;
-				break;
-		case 4:	if(common == 1)	SegVal = 0b01100110;
-				else			SegVal = ~0b01100110;
-				break;
-		case 5:	if(common == 1)	SegVal = 0b01101101;
-				else			SegVal = ~0b01101101;
-				break;
-		case 6:	if(common == 1)	SegVal = 0b01111101;
-				else			SegVal = ~0b01111101;
-				break;
-		case 7:	if(common == 1)	SegVal = 0b00000111;
-				else			SegVal = ~0b00000111;
-				break;
-		case 8:	if(common == 1)	SegVal = 0b01111111;
-				else			SegVal = ~0b01111111;
-				break;
-		case 9:	if(common == 1)	SegVal = 0b01101111;
-				else			SegVal = ~0b01101111;		
+		case 0:	SegVal = 0b00111111; break;
+		case 1:	SegVal = 0b00000110; break;
+		case 2:	SegVal = 0b01011011; break;
+		case 3:	SegVal = 0b01001111; break;
+		case 4:	SegVal = 0b01100110; break;
+		case 5:	SegVal = 0b01101101; break;
+		case 6:	SegVal = 0b01111101; break;
+		case 7:	SegVal = 0b00000111; break;
+		case 8:	SegVal = 0b01111111; break;
+		case 9:	SegVal = 0b01101111; break;
+		case '=':SegVal = 0b01001000; break;
+		case 'a':SegVal = 0b01110111; break;
+		case 'b':SegVal = 0b01111100; break;
+		case ' ':SegVal = 0b00000000; break;
+		case '-':SegVal = 0b01000000; break;
 	}		
+	if (!common) SegVal = ~SegVal;
+	
 	return SegVal;
-}
-
-/*Increment any of six digits based on location*/
-unsigned char Increment(unsigned char digit)
-{
-	if(!calc) {
-		switch(digit) {
-			case 0:
-				if (seconds % 10 < 9) seconds++;
-				else seconds = (seconds / 10) * 10;
-				break;
-			case 1:
-				if (seconds + 10 < 60) seconds += 10;
-				else seconds %= 10;
-				break;
-			case 2:
-				if (minutes % 10 < 9) minutes++;
-				else minutes = (minutes / 10) * 10;
-				break;
-			case 3:
-				if (minutes + 10 < 60) minutes += 10;
-				else minutes %= 10;
-				break;
-			case 4:
-				if (hours % 10 < 9) hours++;
-				else hours = (hours / 10) * 10;
-				break;
-			case 5:
-				if (hours + 10 < 24) hours += 10;
-				else hours %= 10;
-				break;
-		}
-	} else {
-		switch(digit) {
-			case 0: break;	// Can't change result
-			case 1: break;
-			case 2:
-				if (int2 % 10 < 9) int2++;
-				else int2 = (int2 / 10) * 10;
-				break;
-			case 3:
-				if (int2 + 10 < 100) int2 += 10;
-				else int2 %= 10;
-				break;
-			case 4:
-				if (int1 % 10 < 9) int1++;
-				else int1 = (int1 / 10) * 10;
-				break;
-			case 5:
-				if (int1 + 10 < 100) int1 += 10;
-				else int1 %= 10;
-				break;
-		}
-	}
 }
  
 /*Timer Counter 1 Compare Match A Interrupt Service Routine/Interrupt Handler*/
@@ -358,4 +326,67 @@ ISR(TIMER1_COMPA_vect)
 	}
 	if(hours > 23)
 		hours = 0;
+}
+
+/*Read analog data using ADC*/
+uint16_t AnalogRead(uint8_t ch)
+{
+  // select the corresponding channel 0~7
+  // ANDing with ’7′ will always keep the value
+  // of ‘ch’ between 0 and 7
+  ch &= 0b00000111;  // AND operation with 7
+  ADMUX = (ADMUX & 0xF8)|ch; // clears the bottom 3 bits before ORing
+ 
+  // start single convertion
+  // write ’1′ to ADSC
+  ADCSRA |= (1<<ADSC);
+ 
+  // wait for conversion to complete
+  // ADSC becomes ’0′ again
+  // till then, run loop continuously
+  while(ADCSRA & (1<<ADSC));
+ 
+  return (ADC);
+}
+
+/*Map Buttons from ADC Reading*/
+uint8_t Button(uint16_t adc, uint16_t error)
+{
+	uint8_t buts[16] = {  1,  2,  3,'a',  4,  5,  6,'b',  7,  8,  9,'c','*' ,0,'#','d'};
+	uint16_t adcs[16] = {559,541,521,500,480,456,428,398,370,334,293,247,203,145, 78,  0};
+	
+	uint8_t i = 0;
+	uint8_t button = 255;	// Default state, unpressed
+	
+	while((button == 255) && (i < 16)) {
+		if (InRange(adc, adcs[i], error)) button = buts[i];
+		i++;
+	}
+	
+	Serial.print(adc); Serial.print(' '); Serial.print(button); Serial.print(' ');
+	
+	return button;
+}
+
+/*Compare with error*/
+uint8_t InRange(uint16_t in, uint16_t compare, uint16_t error)
+{
+	uint16_t max, min;
+	
+	// Simple overflow prevention,
+	// but can only handle for compare absolute max or min
+	if (compare == 0xFF) max = compare;
+	else max = compare + error;
+	if (compare == 0x00) min = compare;
+	else min = compare - error;
+	
+	if ((in <= max) && (in >= min)) return 1;
+	else return 0;
+}
+
+/*Absolute*/
+uint16_t Abs(int16_t in)
+{
+	if (in >= 0) return in;
+	else return -1*in;
 }
